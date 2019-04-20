fd995ff39a3af5589b61f291b118b763b80696a2
Nicola Bernardini <nicola.bernardini.rome@gmail.com>
2019-04-20 05:24:52 +0200

    added the ftom method

--- lib/ruby/mext/numeric.rb
+++ lib/ruby/mext/numeric.rb
@@ -9,6 +9,7 @@ class Numeric
   # +DEFAULT_PITCH_FORK+ is our 'A' (or +La+) standard
   #
   DEFAULT_PITCH_FORK = 440.0
+  MIDI_PITCH_FORK    = 69.0
   #
   # 
   @@pitch_fork = DEFAULT_PITCH_FORK
@@ -29,6 +30,22 @@ class Numeric
       @@pitch_fork
     end
 
+    #
+    # +reset_pitch_fork+: resets the pitch fork to its default
+    #
+    def reset_pitch_fork
+      @@pitch_fork = DEFAULT_PITCH_FORK
+    end
+
+  end
+
+  #
+  # +mtof+: MIDI note to frequency converter
+  #
+  # interprets its receiver as a MIDI note and returns its frequency in Hertz
+  #
+  def mtof
+    self.class.pitch_fork * (2.0**((self - MIDI_PITCH_FORK)/12.0))
   end
 
 end
--- spec/ruby/mext/numeric_spec.rb
+++ spec/ruby/mext/numeric_spec.rb
@@ -2,9 +2,24 @@ require "spec_helper"
 
 describe Numeric do
 
+  class PitchTest
+    attr_reader :freq, :midi
+
+    def initialize(f, m)
+      @freq = f
+      @midi = m
+    end
+  end
+
   before :example do
     @new_pitch_fork = 442.6
-    @random_pitches = [261.6, -128, 756222.23]
+    @random_pitches =
+    [
+      PitchTest.new(261.6, 60.0),
+      PitchTest.new(-128, 47.624),
+      PitchTest.new(756222.23, 197.965),
+      PitchTest.new(Numeric::DEFAULT_PITCH_FORK, Numeric::MIDI_PITCH_FORK),
+    ]
   end
 
   it 'has a pitch fork setter class method' do
@@ -13,6 +28,15 @@ describe Numeric do
 
   it 'has a pitch fork getter class method' do
     expect(Numeric.respond_to?(:pitch_fork)).to be true
+  end
+
+  it 'has a reset_pitch_fork class method that works' do
+    expect(Numeric.respond_to?(:reset_pitch_fork)).to be true
+    expect(Numeric.reset_pitch_fork).to eq(Numeric::DEFAULT_PITCH_FORK)
+    expect(Numeric.pitch_fork = @new_pitch_fork).to eq(@new_pitch_fork)
+    expect(Numeric.pitch_fork).to eq(@new_pitch_fork)
+    expect(Numeric.reset_pitch_fork).to eq(Numeric::DEFAULT_PITCH_FORK)
+    expect(Numeric.pitch_fork).to eq(Numeric::DEFAULT_PITCH_FORK)
   end
 
   it 'has a pitch fork attribute that works' do
@@ -31,7 +55,15 @@ describe Numeric do
     #
     # numbers see it
     #
-    @random_pitches.each { |rp| expect(rp.class.pitch_fork).to(eq(@new_pitch_fork), rp.to_s) }
+    @random_pitches.each do
+      |rp|
+      expect(rp.freq.class.pitch_fork).to(eq(@new_pitch_fork), rp.freq.to_s)
+      expect(rp.midi.class.pitch_fork).to(eq(@new_pitch_fork), rp.midi.to_s)
+    end
+  end
+
+  it 'has a mtof method that works' do
+
   end
 
 end
