ef272ab0cca90c3dffa56b29bde617ca1ce62031
Nicola Bernardini <nicola.bernardini.rome@gmail.com>
2019-04-20 16:23:46 +0200

    added the mtof method to Numeric

--- lib/ruby/mext/numeric.rb
+++ lib/ruby/mext/numeric.rb
@@ -44,7 +44,10 @@ class Numeric
   #
   # interprets its receiver as a MIDI note and returns its frequency in Hertz
   #
+  class NegativeNumeric < StandardError; end
+
   def mtof
+    raise NegativeNumeric if self < 0.0
     self.class.pitch_fork * (2.0**((self - MIDI_PITCH_FORK)/12.0))
   end
 
--- spec/ruby/mext/numeric_spec.rb
+++ spec/ruby/mext/numeric_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Numeric do
 
-  class PitchTest
+  class NumericTester
     attr_reader :freq, :midi
 
     def initialize(f, m)
@@ -12,13 +12,14 @@ describe Numeric do
   end
 
   before :example do
+    @eps = 1e-6
     @new_pitch_fork = 442.6
     @random_pitches =
     [
-      PitchTest.new(261.6, 60.0),
-      PitchTest.new(-128, 47.624),
-      PitchTest.new(756222.23, 197.965),
-      PitchTest.new(Numeric::DEFAULT_PITCH_FORK, Numeric::MIDI_PITCH_FORK),
+      NumericTester.new(261.6255653005986, 60.0),
+      NumericTester.new(128, 47.62368343770),
+      NumericTester.new(756222.2300000156, 197.96509224598),
+      NumericTester.new(Numeric::DEFAULT_PITCH_FORK, Numeric::MIDI_PITCH_FORK),
     ]
   end
 
@@ -63,7 +64,16 @@ describe Numeric do
   end
 
   it 'has a mtof method that works' do
+    Numeric.reset_pitch_fork
+    @random_pitches.each do
+      |rp|
+      expect(rp.midi.mtof).to(be_within(@eps).of(rp.freq), "#{rp.midi.mtof} != #{rp.freq}")
+    end
+  end
 
+  it 'raises an Exception within mtof if the receiver is negative' do
+    Numeric.reset_pitch_fork
+    expect { -128.mtof }.to raise_error(Numeric::NegativeNumeric)
   end
 
 end
